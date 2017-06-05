2d13d9984e4a311ff3718410eda06b7001d1675e
Ryan Lue <ryan.lue@gmail.com>
2017-06-05 11:47:06 +0800

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file,
+  # see http://guides.rubyonrails.org/routing.html
+  HighVoltage.configure do |config|
+    config.home_page = 'home'
+    config.route_drawer = HighVoltage::RouteDrawers::Root
+  end
 end
